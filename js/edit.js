28b8085339afa6e5c25490da8ca4bfbceccb1449
mohamed tamaa <mtamaaa@gmail.com>
2019-07-13 18:09:58 +0200

    neew

--- js/edit.js
+++ js/edit.js
@@ -1,5 +1,9 @@
-  $(document).ready(function() {
+//halls  
+$(document).ready(function() {
     $('#example').DataTable( {
+        
+        "scrollX": true,
+        
         "ajax": {
             "url": "https://hidden-ocean-87285.herokuapp.com/halls/listHalls",
             "type": "POST"
@@ -10,7 +14,47 @@
             { "data": "hallCategory.name" },
             { "data": "hallsAverageRating" },
             { "data": "hallPrice" },
-            { "data": "hallPhoneNumber" }
+            { "data": "hallPhoneNumber" },
+            { "data": "hallImage" },
+            { "data": "hallAdress" },
+            { "data": "hallDescription" },
+            { "data": "hallSpecialOffers" }
+            
+            
+            
+            
         ]
     } );
-  } );
+      
+  });
+//users 
+                                                //need token//
+  $(document).ready(function() {
+    $('#exampleUsers').DataTable( {
+        "scrollX": true,
+        "ajax": {
+            "url": "https://hidden-ocean-87285.herokuapp.com/users",
+            "type": "POST",
+        
+        },
+        "columns": [
+            { "data": "_id" },
+            { "data": "userName" },
+            { "data": "userEmail" }
+        ]
+    } );
+      
+  });
+                                                //need token//
+$(document).ready(function()
+ {
+    $(".bars").click(function()
+    { 
+      $('.bars i').toggleClass('fa-times')
+      $(".main").toggleClass("custom");
+  });
+  });
+
+
+
+
