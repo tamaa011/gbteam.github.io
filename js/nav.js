b726429b7c62be4aeb6e0b2f022c4f2fc6efa41d
AhmedElsayedAhmed <fcih_ahmed@hotmail.com>
2019-08-17 15:36:15 +0200

    Update nav.js

--- js/nav.js
+++ js/nav.js
@@ -3,12 +3,12 @@ $(document).ready(function () {
     var actions = JSON.parse(localStorage.getItem("actions"));
     var username = localStorage.getItem("User");
     $("#username").val(username);
-$.each( loc[0].Admin, function( key, value ) {
+$.each( actions[0].Admin, function( key, value ) {
 });
-$.each( loc[0].Halls, function( key, value ) {
+$.each( actions[0].Halls, function( key, value ) {
     $(".HallsLink").append('<a href="listallhalls.html">List halls</a>')
 });
-$.each( loc[0].Users, function( key, value ) {
+$.each( actions[0].Users, function( key, value ) {
 });
      
 });
