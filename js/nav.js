02d0351f356250bf183835ba53df8bed472dd7ac
AhmedElsayedAhmed <fcih_ahmed@hotmail.com>
2019-08-17 17:18:04 +0200

    change names of html pages

--- js/nav.js
+++ js/nav.js
@@ -3,14 +3,18 @@ $(document).ready(function () {
     var actions = JSON.parse(localStorage.getItem("actions"));
     var username = localStorage.getItem("User");
     $("#username").html(username);
-$.each( actions[0].Admin, function( key, value ) {
-    
 
+$.each( actions[0].Admin, function( key, value ) {
+    $(".adminlink").append('<a href="'+ value.split(' ').join('')+'.html">'+ value + '</a><hr>');
 });
+
 $.each( actions[0].Halls, function( key, value ) {
     $(".HallsLink").append('<a href="'+ value.split(' ').join('')+'.html">'+ value + '</a><hr>');
 });
+
 $.each( actions[0].Users, function( key, value ) {
+    $(".userslink").append('<a href="'+ value.split(' ').join('')+'.html">'+ value + '</a><hr>');
+
 });
      
 });
