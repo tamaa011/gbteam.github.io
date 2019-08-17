b484d0abc0cf4b01a498937795d6ce0c80202159
AhmedElsayedAhmed <fcih_ahmed@hotmail.com>
2019-08-17 17:00:32 +0200

    Update nav.js

--- js/nav.js
+++ js/nav.js
@@ -8,7 +8,7 @@ $.each( actions[0].Admin, function( key, value ) {
 
 });
 $.each( actions[0].Halls, function( key, value ) {
-    $(".HallsLink").append('<a href="'+ value.split(' ').join('')+'">'+ value + '</a>');
+    $(".HallsLink").append('<a href="'+ value.split(' ').join('')+'.html">'+ value + '</a><hr>');
 });
 $.each( actions[0].Users, function( key, value ) {
 });
